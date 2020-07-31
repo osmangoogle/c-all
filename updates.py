04df56f9b078061de2c43fda95b256f8e0ce14da
Angga Kurniawan <67410461+anggaxd@users.noreply.github.com>
2020-07-31 21:39:46 +0700

    Update updates.py

--- updates.py
+++ updates.py
@@ -6,3 +6,18 @@
 ## * Release : 31-07-20,05.00           ##
 ## * Update  : (+) Code Area Country    ##
 ##======================================##
+#### LIST COUNTRY FOR CLONING ####    
+        [1]\033[1;97m  Bangladesh'
+	[2]\033[1;97m  USA'
+	[3]\033[1;97m  UK'
+	[4] \033[1;97m India'
+	[5]\033[1;97m  Brazil'
+	[6]\033[1;97m  Japan'
+	[7]\033[1;97m  Korea'
+	[8]\033[1;97m  Italy'
+	[9]\033[1;97m  Spain'
+	[10]\033[1;97m Poland'
+	[11]\033[1;97m Pakistan'
+	[12]\033[1;97m Indonesia'
+	[13]\033[1;97m Iran'
+	[14]\033[1;97m Grecee'
